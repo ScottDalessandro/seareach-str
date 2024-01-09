1e7863633626330ad10c808bec51ce2a12c3246d
Scott D'Alessandro <sdaless22@gmail.com>
2024-01-09 09:12:23 -0500

    Feature/no ref/footer (#4)
    * add footer scaffold, update file structure
    
    * add tertiary-dark global to Header
    
    * add footer stylings, remove global & boiler code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
-import Header from '@/components/Header';
+import Header from '@/components/Header/Header';
+import Footer from '@/components/Footer/Footer';
 import type { Metadata } from 'next'
 import { Poppins } from 'next/font/google'
 import './globals.css'
@@ -27,7 +28,7 @@ export default function RootLayout({
         <main className='font-normal'>
           <Header/>
           {children}
-          {/* ?Footer */}
+          <Footer/>
         </main>
         </body>
     </html>
