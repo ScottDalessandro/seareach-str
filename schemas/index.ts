3e1c06890c25b11a0e1085aa151848a27c77f1de
Scott D'Alessandro <scott@fullstackacademy.com>
2024-01-19 16:20:49 -0500

    add verificationToken schema

--- schemas/index.ts
+++ schemas/index.ts
@@ -3,6 +3,7 @@ import booking from './booking'
 import review from './review'
 import property from './property'
 import user from './user'
+import verificationToken from './verificationToken'
 
 
-export const schemaTypes = [account, booking, review, property, user]
+export const schemaTypes = [account, booking, review, property, user, verificationToken]
