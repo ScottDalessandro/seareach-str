a1f6691955bc8fd08eec313a43da7eab99caccaa
Scott D'Alessandro <scott@fullstackacademy.com>
2024-01-19 15:13:41 -0500

    add property schema

--- schemas/index.ts
+++ schemas/index.ts
@@ -1,5 +1,6 @@
 import account from './account'
 import booking from './booking'
+import property from './property'
 import user from './user'
 
-export const schemaTypes = [user, account, booking]
+export const schemaTypes = [account, booking, property, user]
