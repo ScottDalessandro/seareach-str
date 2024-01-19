2f43b85cea1018ef8887fd8d0b7243bc8e075bc6
Scott D'Alessandro <sdaless22@gmail.com>
2024-01-19 16:21:23 -0500

    Feature/no ref/sanity schemas (#8)
    * install sanity-auth package
    
    * add user schema
    
    * add account schema
    
    * add booking schema
    
    * update rentalProperty to property
    
    * add property schema
    
    * add review type
    
    * add review schema
    
    * add verificationToken schema

--- schemas/index.ts
+++ schemas/index.ts
@@ -1,1 +1,9 @@
-export const schemaTypes = []
+import account from './account'
+import booking from './booking'
+import review from './review'
+import property from './property'
+import user from './user'
+import verificationToken from './verificationToken'
+
+
+export const schemaTypes = [account, booking, review, property, user, verificationToken]
