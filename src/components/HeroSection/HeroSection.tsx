2d08836127d420cc77863ba0e98a6b6f7ea0734f
Scott D'Alessandro <scott@fullstackacademy.com>
2024-01-22 15:56:06 -0500

    add basic structure of hero section

---
+++ src/components/HeroSection/HeroSection.tsx
@@ -0,0 +1,11 @@
+const HeroSection = () => {
+  return (
+    <section className="flex px-4 items-center gap-12 container mx-auto">
+      <div className="py-10 h-full">
+        {/* Heading */}
+        {/* Room Description */}
+      </div>
+      {/* IMAGES */}
+    </section>
+  );
+};
