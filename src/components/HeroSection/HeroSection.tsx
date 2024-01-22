463d8d7f616aa55456b78d959f226fef6952d9a3
Scott D'Alessandro <scott@fullstackacademy.com>
2024-01-22 16:17:05 -0500

    add heading and new css classes

--- src/components/HeroSection/HeroSection.tsx
+++ src/components/HeroSection/HeroSection.tsx
@@ -2,10 +2,16 @@ const HeroSection = () => {
   return (
     <section className="flex px-4 items-center gap-12 container mx-auto">
       <div className="py-10 h-full">
-        {/* Heading */}
+        <h1 className="font-heading mb-6">Explore our Exquisite Rental</h1>
+        <p className="text-[#4a4a4a] dark:text-[#ffffffea] mb-12 max-w-lg">
+          Experience an Exquisite Stay at Point Pleasant Beach!
+        </p>
+        <button className="btn-primary">Get Started</button>
         {/* Room Description */}
       </div>
       {/* IMAGES */}
     </section>
   );
 };
+
+export default HeroSection;
